f8426595f22e23c77b95fa07ec6483742155720a
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2024-01-06 03:23:55 +0900

    C++언어 BAEKJOON 10172번 문제 풀기!(1)

---
+++ CPP_10172_1/CPP_10172_1.cpp
@@ -0,0 +1,21 @@
+/*
+아래 예제와 같이 개를 출력하시오.
+
+없음.
+
+개를 출력한다.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    cout << "|\\_/|" << endl;
+    cout << "|q p|   /}" << endl;
+    cout << "( 0 )\"\"\"\\" << endl;
+    cout << "|\"^\"`    |" << endl;
+    cout << "||_/=\\\\__|" << endl;
+
+    return 0;
+}
