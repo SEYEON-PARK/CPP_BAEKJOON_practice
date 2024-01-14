267b272bc6973180403ee9b8896378d9e25c561e
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2024-01-14 23:27:49 +0900

    C++언어 BAEKJOON 1330번 문제 풀기!(1)

--- CPP_1330_1/CPP_1330_1.cpp
+++ CPP_1330_1/CPP_1330_1.cpp
@@ -8,3 +8,20 @@
 - A가 B보다 작은 경우에는 '<'를 출력한다.
 - A와 B가 같은 경우에는 '=='를 출력한다.
 */
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    int A, B;
+    cin >> A >> B;
+    if(A>B)
+        cout << '>';
+    else if(A<B)
+        cout << '<';
+    else
+        cout << "==";
+    
+    return 0;
+}
