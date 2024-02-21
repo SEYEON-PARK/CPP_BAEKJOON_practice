1b867f0c4f92fc393a024e410829df362db8d1c8
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2024-02-21 22:33:43 +0900

    C++언어 BAEKJOON 10926번 문제 풀기!(1)

--- CPP_10926_1/CPP_10926_1.cpp
+++ CPP_10926_1/CPP_10926_1.cpp
@@ -6,3 +6,15 @@
 
 첫째 줄에 준하의 놀람을 출력한다. 놀람은 아이디 뒤에 ??!를 붙여서 나타낸다.
 */
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    string id;
+    cin >> id;
+    cout << id << "??!";
+    
+    return 0;
+}
