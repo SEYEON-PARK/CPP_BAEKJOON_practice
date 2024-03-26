21ff9f3d9ee80769e0a9822bf3b4705f3cc13af7
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2024-03-26 20:23:23 +0900

    C++언어 BAEKJOON 14681번 문제 풀기!(1)

--- CPP_14681_1/CPP_14681_1.cpp
+++ CPP_14681_1/CPP_14681_1.cpp
@@ -7,3 +7,30 @@
 
 점 (x, y)의 사분면 번호(1, 2, 3, 4 중 하나)를 출력한다.
 */
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    int x, y;
+    cin >> x;
+    cin >> y;
+    
+    if(x>0)
+    {
+        if(y>0)
+            cout << "1";
+        else
+            cout << "4";
+    }
+    else
+    {
+        if(y>0)
+            cout << "2";
+        else
+            cout << "3";
+    }
+    
+    return 0;
+}
