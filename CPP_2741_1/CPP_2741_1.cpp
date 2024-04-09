2ea967f0a58cc345d83d5e9705f1b813213fef37
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2024-04-09 21:00:58 +0900

    C++언어 BAEKJOON 2741번 문제 풀기!(1)

--- CPP_2741_1/CPP_2741_1.cpp
+++ CPP_2741_1/CPP_2741_1.cpp
@@ -5,3 +5,19 @@
 
 첫째 줄부터 N번째 줄 까지 차례대로 출력한다.
 */
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    int N;
+    cin >> N;
+    
+    for(int i=1;i<=N;i++)
+    {
+        cout << i << '\n';
+    }
+    
+    return 0;
+}  
