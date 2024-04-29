b566e389889c983589ac2f790b8836acea210d7f
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2024-04-29 22:41:16 +0900

    C++언어 BAEKJOON 2438번 문제 풀기!(1)

--- CPP_2438_1/CPP_2438_1.cpp
+++ CPP_2438_1/CPP_2438_1.cpp
@@ -5,3 +5,23 @@
 
 첫째 줄부터 N번째 줄까지 차례대로 별을 출력한다.
 */
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    int num;
+    cin >> num;
+    
+    for(int i=0;i<num;i++)
+    {
+        for(int j=0;j<=i;j++)
+        {
+            cout << '*';
+        }
+        cout << '\n';
+    }
+    
+    return 0;
+}
