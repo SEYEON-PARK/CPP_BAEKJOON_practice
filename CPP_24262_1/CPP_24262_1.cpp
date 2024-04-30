398ff3b79021a63b8d0bf12befa6199f62bbe0d3
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2024-05-01 01:38:01 +0900

    C++언어 BAEKJOON 24262번 문제 풀기!(1)

--- CPP_24262_1/CPP_24262_1.cpp
+++ CPP_24262_1/CPP_24262_1.cpp
@@ -12,3 +12,16 @@ MenOfPassion(A[], n) {
 첫째 줄에 코드1 의 수행 횟수를 출력한다.
 둘째 줄에 코드1의 수행 횟수를 다항식으로 나타내었을 때, 최고차항의 차수를 출력한다. 단, 다항식으로 나타낼 수 없거나 최고차항의 차수가 3보다 크면 4를 출력한다.
 */
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    int n;
+    cin >> n;
+    
+    cout << '1' << endl << '0';
+    
+    return 0;
+}
