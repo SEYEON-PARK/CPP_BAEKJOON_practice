cde51228122407684b1409bb0aec51dba5b841ea
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2024-05-26 22:45:38 +0900

    C++언어 BAEKJOON 11021번 문제 풀기!(1)

--- CPP_11021_1/CPP_11021_1.cpp
+++ CPP_11021_1/CPP_11021_1.cpp
@@ -6,3 +6,21 @@
 
 각 테스트 케이스마다 "Case #x: "를 출력한 다음, A+B를 출력한다. 테스트 케이스 번호는 1부터 시작한다.
 */
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    int N;
+    cin >> N;
+    
+    for(int i=0;i<N;i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        cout << "Case #" << i+1 << ": " << a+b << endl;
+    }
+    
+    return 0;
+}
