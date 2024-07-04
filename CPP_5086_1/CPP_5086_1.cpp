8eca67d23adb0d59fc2d0abc2a0309378154b37f
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2024-07-04 18:05:23 +0900

    C++언어 BAEKJOON 5086번 문제 풀기!(1)

--- CPP_5086_1/CPP_5086_1.cpp
+++ CPP_5086_1/CPP_5086_1.cpp
@@ -12,3 +12,26 @@
 
 각 테스트 케이스마다 첫 번째 숫자가 두 번째 숫자의 약수라면 factor를, 배수라면 multiple을, 둘 다 아니라면 neither를 출력한다.
 */
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    int a, b;
+    cin >> a >> b;
+    
+    while(a != 0 && b != 0)
+    {
+        if(a % b == 0)
+            cout << "multiple\n";
+        else if(b % a == 0)
+            cout << "factor\n";
+        else
+            cout << "neither\n";
+        
+        cin >> a >> b;
+    }
+    
+    return 0;
+}
