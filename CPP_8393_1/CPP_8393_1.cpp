3ca39f87898a1a8fabff7ad638b0b3421f372b47
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2025-02-06 18:48:23 +0900

    C++언어 BAEKJOON 8393번 문제 풀기!(1)

--- CPP_8393_1/CPP_8393_1.cpp
+++ CPP_8393_1/CPP_8393_1.cpp
@@ -11,16 +11,16 @@ using namespace std;
 
 int main(void)
 {
-    int n, N, sum=0;
-    cin >> n;
-    N=n;
+    int n, N, sum=0; // 변수 선언 및 초기화
+    cin >> n; // 사용자로부터 정수 입력받기
+    N=n; // N에 n 대입하기
     
-    for(int i=0;i<n;i++)
+    for(int i=0;i<n;i++) // i가 0부터 n이 되기 전까지 1씩 증가하며 반복
     {
-        sum+=N--;
+        sum+=N--; // sum에 sum+N을 대입한 후, N에서 1 빼기
     }
     
-    cout << sum;
+    cout << sum; // 결과 출력하기
     
     return 0;
 }
