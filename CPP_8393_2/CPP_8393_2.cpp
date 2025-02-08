14ee490a7039d31446e8b021817e1f1a22630b2a
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2025-02-08 23:52:37 +0900

    C++언어 BAEKJOON 8393번 문제 풀기!(2)

--- CPP_8393_2/CPP_8393_2.cpp
+++ CPP_8393_2/CPP_8393_2.cpp
@@ -11,15 +11,15 @@ using namespace std;
 
 int main(void)
 {
-    int n, sum=0;
-    cin >> n;
+    int n, sum=0; // 변수 선언 및 초기화
+    cin >> n; // 사용자로부터 정수 입력받기
     
-    for(int i=1;i<=n;i++)
+    for(int i=1;i<=n;i++) // i가 1부터 n이 될 때까지 1씩 증가시키며 반복
     {
-        sum+=i;
+        sum+=i; // sum에 sum+i를 대입하기
     }
     
-    cout << sum;
+    cout << sum; // 결과 출력하기
     
     return 0;
 }
