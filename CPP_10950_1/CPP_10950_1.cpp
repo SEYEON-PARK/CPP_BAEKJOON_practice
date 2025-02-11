6e259ae35e03ab581b43eaadd5410421467f26b2
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2025-02-11 22:46:16 +0900

    C++언어 BAEKJOON 10950번 문제 풀기!(1)

--- CPP_10950_1/CPP_10950_1.cpp
+++ CPP_10950_1/CPP_10950_1.cpp
@@ -12,13 +12,13 @@ using namespace std;
 
 int main(void)
 {
-    int T, A, B;
-    cin >> T;
+    int T, A, B; // 변수 선언
+    cin >> T; // 사용자로부터 정수 입력받기
     
-    for (int i=0;i<T;i++)
+    for (int i=0;i<T;i++) // i가 0부터 T가 되기 전까지 1씩 증가시키며 반복
     {
-        cin >> A >> B;
-        cout << A+B << '\n';
+        cin >> A >> B; // 사용자로부터 두 개의 정수 입력받기
+        cout << A+B << '\n'; // 결과 출력하기
     }
     
     return 0;
