bd410040f936a893bb0121812c8e0e04818b9151
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2025-02-17 00:12:02 +0900

    C++언어 BAEKJOON 1085번 문제 풀기!(1)

--- CPP_1085_1/CPP_1085_1.cpp
+++ CPP_1085_1/CPP_1085_1.cpp
@@ -12,24 +12,25 @@ using namespace std;
 
 int main(void)
 {
-    int x, y, w, h, min;
-    int r[4];
+    int x, y, w, h, min; // 변수 선언
+    int r[4]; // 배열 선언
     
-    cin >> x >> y >> w >> h;
-    
+    cin >> x >> y >> w >> h; // 사용자로부터 네 개의 정수 입력받기
+
+    // 배열에 각 정수 대입하기
     r[0]=x;
     r[1]=y;
     r[2]=w-x;
     r[3]=h-y;
     min=r[0];
     
-    for(int i=0;i<4;i++)
+    for(int i=0;i<4;i++) // i에 0부터 4가 되기 전까지 1씩 증가시킨 값을 대입하며 반복
     {
-        if(r[i]<min)
-            min=r[i];
+        if(r[i]<min) // 만약, r[i]가 min보다 크다면
+            min=r[i]; // min에 r[i] 대입하기
     }
     
-    cout << min;
+    cout << min; // 결과 출력하기
     
     return 0;
 }
