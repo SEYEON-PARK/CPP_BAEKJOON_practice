be33e911a347d15999101b76dcb2c0c7e0a9e085
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2025-02-18 11:37:13 +0900

    C++언어 BAEKJOON 2444번 문제 풀기!(1)

---
+++ CPP_2444_1/CPP_2444_1.cpp
@@ -0,0 +1,51 @@
+/*
+예제를 보고 규칙을 유추한 뒤에 별을 찍어 보세요.
+
+첫째 줄에 N(1 ≤ N ≤ 100)이 주어진다.
+
+첫째 줄부터 2×N-1번째 줄까지 차례대로 별을 출력한다.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    int N, minus=3;
+    cin >> N;
+    
+    for(int i=0;i<2*N-1;i++)
+    {
+        if(i<=N-1)
+        {
+            for(int j=0;j<N-i-1;j++)
+            {
+                cout << ' ';
+            }
+            for(int k=0;k<2*(i+1)-1;k++)
+            {
+                cout << '*';
+            }
+            
+            cout << '\n';
+        }
+        else
+        {
+            for(int j=0;j<i-N+1;j++)
+            {
+                cout << ' ';
+            }
+            for(int k=2*N-minus;k>0;k--)
+            {
+                cout << '*';
+            }
+            minus+=2;
+            
+            cout << '\n';
+        }
+        
+        
+    }
+    
+    return 0;
+}
