48858d65689d5e4d065347a2254b5d4857899d36
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2025-02-21 22:36:43 +0900

    C++언어 BAEKJOON 2444번 문제 풀기!(1)

--- CPP_2444_1/CPP_2444_1.cpp
+++ CPP_2444_1/CPP_2444_1.cpp
@@ -14,9 +14,9 @@ int main(void)
     int N, minus=3; // 변수 선언 및 초기화
     cin >> N; // 사용자로부터 정수 입력받기
     
-    for(int i=0;i<2*N-1;i++)
+    for(int i=0;i<2*N-1;i++) // i에 0부터 2*N-1이 되기 전까지 1씩 증가시킨 값을 대입하며 반복
     {
-        if(i<=N-1)
+        if(i<=N-1) // 만약, i가 N-1보다 작거나 같다면
         {
             for(int j=0;j<N-i-1;j++)
             {
